db58f9d60e2745aa6e81581d54cd7074149bf8f6
le roncier <leroncier@gmail.com>
2016-01-21 20:24:23 +0100

    Initial Commit

---
+++ Attack of the Constants and Variables/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Attack of the Constants and Variables
+//
+//  Created by Charles on 21/01/2016.
+//  Copyright © 2016 Charles Roncier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
